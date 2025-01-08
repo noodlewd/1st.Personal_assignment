e162b60083413e5f42a7fdbb88257ea8abc9d845
noodlewd <michel35@naver.com>
2025-01-08 23:25:10 +0900

    html,css 수정 및 js api 데이터 값 넣기

--- api.js
+++ api.js
@@ -10,7 +10,14 @@ const options = {
 
 fetch(url, options)
   .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error(err));
+  // api로 받아온 데이터를 html에 각 위치에 뿌려줌
+  .then((data) => {
+    // TMDB에서 가져온 기초 이미지 src를 절대경로로 바꿔주는 작업
+    const baseUrl = "https://image.tmdb.org/t/p/w200";
+    document.querySelector(".movie_img").src = baseUrl + data.results[0].poster_path; // 이미지 path
+    document.querySelector(".movie_name").innerHTML = data.results[0].title; // 영화 제목
+    document.querySelector(".cus_avg").innerHTML = "평점: " + data.results[0].vote_average.toFixed(1) + "점"; // 관객 평점
+  })
+  .catch((err) => console.error(err)); // 비동기 작업 중 발생한 오류를 감지하는 코드(좀 더 공부 해볼 것.)
 
 //검색
