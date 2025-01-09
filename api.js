bdac2e8508cd0bfa6e359f121890e79b738180e2
noodlewd <michel35@naver.com>
2025-01-09 17:59:29 +0900

    forEach api 데이터 반복 및 modal op,cl 구현

--- api.js
+++ api.js
@@ -10,14 +10,50 @@ const options = {
 
 fetch(url, options)
   .then((res) => res.json())
-  // api로 받아온 데이터를 html에 각 위치에 뿌려줌
   .then((data) => {
-    // TMDB에서 가져온 기초 이미지 src를 절대경로로 바꿔주는 작업
     const baseUrl = "https://image.tmdb.org/t/p/w200";
-    document.querySelector(".movie_img").src = baseUrl + data.results[0].poster_path; // 이미지 path
-    document.querySelector(".movie_name").innerHTML = data.results[0].title; // 영화 제목
-    document.querySelector(".cus_avg").innerHTML = "평점: " + data.results[0].vote_average.toFixed(1) + "점"; // 관객 평점
+    const container = document.querySelector(".container");
+
+    // 데이터 검증단계 - api 데이터인 data.results 배열이 잘 넘어오는지 확인하는 방법
+    if (!Array.isArray(data.results) || data.results.length === 0) {
+      console.error("데이터가 없거나 잘못된 구조입니다.");
+      return;
+    }
+
+    // 기존 콘텐츠 초기화(중복 방지)
+    container.innerHTML = "";
+
+    // data.results배열을 movie 를 통해서 순회
+    data.results.forEach((movie) => {
+      const item = document.createElement("div");
+      item.classList.add("item");
+
+      // 데이터 처리 - 삼항 연산자를 사용해서 이미지가 안넘어 올 시 예비 이미지 사용
+      const imageSrc = movie.poster_path ? baseUrl + movie.poster_path : "placeholder.jpg";
+
+      // HTML 설정 - 백틱을 이옹하여 추가
+      item.innerHTML = `
+        <img class="movie_img" src="${imageSrc}" alt="${movie.title}">
+        <p class="movie_name">${movie.title}</p>
+        <p class="cus_avg">평점: ${movie.vote_average.toFixed(1)}점</p>
+      `;
+      container.appendChild(item);
+
+      // 클릭 이벤트
+      const openModal = document.querySelector(".movie_img");
+      const closeModal = document.querySelector("#close_btn");
+      const modalBox = document.querySelector(".modal");
+
+      openModal.addEventListener("click", () => {
+        modalBox.classList.add("active");
+      });
+
+      closeModal.addEventListener("click", () => {
+        modalBox.classList.remove("active");
+      });
+    });
   })
-  .catch((err) => console.error(err)); // 비동기 작업 중 발생한 오류를 감지하는 코드(좀 더 공부 해볼 것.)
+
+  .catch((err) => console.error("API 호출 오류:", err));
 
 //검색
